84faa5d7c0c86f0c04434b22f6d014bde8168442
Jordan Vlieg <jordanvlieg@gmail.com>
2017-07-21 16:08:33 -0700

    fixed bug

--- tests/test_worker_basic.py
+++ tests/test_worker_basic.py
@@ -2,7 +2,6 @@
 import unittest
 import codecs
 import os
-import 
 
 from workers.basic_worker import BasicUserParseWorker
 
@@ -35,7 +34,7 @@ class TestWorkerBasic(unittest.TestCase):
         with codecs.open(file_path, encoding='utf-8') as f:
             text = f.read()
 
-        results, next_page = worker.parse_text(str(text).strip().replace("\r\n", ''))
+        results, next_page = worker.parse_text(str(text).strip().replace('\r\n', ''))
 
         self.assertGreater(len(results), 0)     # Check that results are returned
         self.assertEqual(len(results[0]), 3)    # Check that results are in triplets (check formatting)
